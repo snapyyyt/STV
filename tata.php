05b06d2fc9bd08e470c995e8a8c0040294b8bf5e
snapyyyt <147979586+snapyyyt@users.noreply.github.com>
2023-10-30 23:32:12 -0700

    Update tata.php

--- tata.php
+++ tata.php
@@ -24,7 +24,7 @@
         "group-title": "Kids",
         "channel-name": "SHINCHAN 24/7",
         "tvg-id": "",
-        "video-streaming-link": "http:\/\/starshare.live:8080\/live\/Hanny\/Hanny\/147724.m3u8",
+        "video-streaming-link": "http:\/\/starshare.live:8080\/live\/Asif@24680\/Asif@24680\/147724.m3u8",
         "license-type": "widevine",
         "license-key": "https:\/\/cwip-shaka-proxy.appspot.com\/no_auth",
         "license-key-local": ""
