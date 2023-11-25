4c762472e6269e0da809eafe547b7e4efea33661
snapyyyt <147979586+snapyyyt@users.noreply.github.com>
2023-11-25 08:54:47 -0700

    Update tata.php

--- tata.php
+++ tata.php
@@ -54,7 +54,7 @@
     "group-title": "Kids",
     "channel-name": "Hungama",
     "tvg-id": "",
-    "video-streaming-link": "https:\/\/sanjibweb.me\/paid\/smtv2\/index.m3u8?id=891",
+    "video-streaming-link": "",
     "license-type": "widevine",
     "license-key": "https:\/\/proxy.uat.widevine.com\/proxy?provider=widevine_test",
     "license-key-local": ""
